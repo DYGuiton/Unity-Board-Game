4103d7bad21ec8c8d6428b689e8e357c36cf4a5d
DYGuiton <33556480+DYGuiton@users.noreply.github.com>
2019-03-18 00:00:57 -0700

    Working on Selection
    Framework for user selection implemented. Need to implement indication and camera panning next.

--- Prototype/Board Game Prototype/Assets/Scripts/GameManager.cs
+++ Prototype/Board Game Prototype/Assets/Scripts/GameManager.cs
@@ -8,11 +8,13 @@ public class GameManager : MonoBehaviour {
 
     MapController Map_Controller;
     public int MapSize = 10;
+    public GameObject currentPlayer;
 
     void Start() {
         Map_Controller = GameObject.Find("Map").GetComponent<MapController>();
         Map_Controller.generateFieldBlueprint(MapSize);
         Map_Controller.generateField();
+        currentPlayer.transform.Find("PlayerController").GetComponent<PlayerControl>().setMyTurn(true);
     }
 
     void Update() {
---
+++ Prototype/Board Game Prototype/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public Camera ViewCamera;
+    TownTile myTown;
+
+    public GameObject currentSelection;
+
+    public bool myTurn { get; set; }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        handleUserInterfacing();
+    }
+
+    private void handleUserInterfacing() {
+        if (!myTurn) {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            Ray ray = ViewCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+
+                Debug.Log("We Hit: " + hit.collider.name + "\n at position: " + hit.point);
+
+                //If we hit nothing, set our selection to null
+
+                //Highlight the clicked object and set it to our currentSelection
+            }
+        }
+    }
+
+    public void setMyTurn(bool isMyTurn) {
+        myTurn = isMyTurn;
+        Camera.main.enabled = !isMyTurn;
+        ViewCamera.enabled = isMyTurn;
+    }
+}
