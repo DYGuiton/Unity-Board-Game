e3344189d393f5fd528fb97233f3d25265292342
DYGuiton <33556480+DYGuiton@users.noreply.github.com>
2019-06-30 02:36:39 -0700

    [PROTO-11]
    Basic Mock-up done. Click select, and scene change tbc

---
+++ Prototype/Board Game Prototype/Assets/Scripts/Main Menu/MenuAnimatorFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAnimatorFunctions : MonoBehaviour
+{
+    [SerializeField] MenuButtonController menuButtonController;
+    public bool disableOnce;
+
+    void PlaySound(AudioClip whichSound) {
+        if (!disableOnce) {
+            menuButtonController.audioSource.PlayOneShot(whichSound);
+        }
+        else {
+            disableOnce = false;
+        }
+    }
+
+}
